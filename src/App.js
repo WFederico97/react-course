5f82ebd2236fc2b4630aaa8dde45e73d6d5bc71b
Federico W <wuthrich09@gmail.com>
2023-05-08 15:27:46 -0300

    First comit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
 import './App.css';
+import UserForm from './components/cardForm/cardForm';
+import Expenses from './components/expenses/expenses';
 
 function App() {
+  const users = [
+    {
+      id : 0,
+      name: "John Doe",
+      age: 30,
+      date: new Date(2022,7,9)
+    },
+    {
+      id : 1,
+      name: "Tomas Smith",
+      age: 50,
+      date: new Date(2022, 9, 15)
+    },
+    {
+      id : 2,
+      name: "Jack Graham",
+      age: 22,
+      date: new Date(2023,1,23)
+    },
+    {
+      id : 3,
+      name: "Charles Vane	",
+      age: 31,
+      date: new Date(2023,2,12)
+    }
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <UserForm users={users} />
     </div>
   );
 }
