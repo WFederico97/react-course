a2d45a8a1ea4610f4c16a85345122e8ac2fb516b
Federico W <wuthrich09@gmail.com>
2023-05-10 18:54:48 -0300

    new commit

--- src/components/todo/form.jsx
+++ src/components/todo/form.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
-import {TextField} from 
+import { TextField, Button } from '@mui/material'
 export default function Form() {
-  return (
+    return (
 
-    <>
-    <form>
-    <TextField type="text" name="title" label="titulo"/>
-    <TextField type="text" name="description" label="Descripcion"/>
-    <Button 
-    variant="contained"
-    >
-        enviar
-    </Button>
-    </form>
+        <>
+            <form>
+                <TextField type="text" name="title" label="titulo" />
+                <TextField type="text" name="description" label="Descripcion" />
+                <Button
+                    variant="contained"
+                    type="submit"
+                >
+                    enviar
+                </Button>
+            </form>
 
-    </>
-  )
+        </>
+    )
 }
--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
+import { TextField, Button, Grid } from "@mui/material";
 import Form from "./form";
 export let TodoList = () => {
-  const [lista, setLista] = useState([]);
-  const onSubmit = () => {
-    console.log("onSubmit");
-  }
-  return (
-    <>
-      <Grid container spacing={2}>
-        <Grid item display="flex" direction="column" gap="20px">
-          <Form onSubmit={onSubmit} />
-        </Grid>
-      </Grid>
-    </>
-  );
+    const [lista, setLista] = useState([]);  ///state hook
+    const onSubmit = () => {
+        console.log("onSubmit");
+    };
+    return (
+        <>
+            <Grid container spacing={2}>
+                <Grid item display="flex" direction="column" gap="20px">
+                    <Form onSubmit={onSubmit} />
+                </Grid>
+            </Grid>
+        </>
+    );
 };
